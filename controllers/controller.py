f41b660c9af5a901738cb1938fbfa859c51498b2
CordiiT14 <cordelia.toennies@live.com>
2022-03-10 13:45:53 +0000

    completed mvp of flask lab

---
+++ controllers/controller.py
@@ -0,0 +1,20 @@
+from flask import render_template, request 
+from app import app
+from models.events_list import event_list, add_new_event
+from models.event import Event 
+
+@app.route('/events')
+def index():
+    return render_template('index.html', title="Home", event_list=event_list)
+
+@app.route('/events', methods=['POST'])
+def add_event():
+    event_name = request.form['event_name']
+    date = request.form['date']
+    room_location = request.form['room_location']
+    number_guests = request.form['number_guests']
+    description = request.form['description']
+    recurring = True if request.form['recurring'] else False
+    new_event = Event(date, event_name, number_guests, room_location, description, recurring)
+    add_new_event(new_event)
+    return render_template('index.html', title="Home", event_list=event_list)
---
+++ models/events_list.py
@@ -0,0 +1,12 @@
+from models.event import Event
+import datetime
+
+event1 = Event(datetime.date(2022, 3, 10), "Project Presentation", 20, "codeclan 1", "Presentation of final projects from E56", False)
+
+event_list = [event1,]
+
+def add_new_event(event):
+    event_list.append(event)
+
+# def recurring_event(recurring):
+#     if recurring == 
